aa97241a3eadfd66e9852c9165ebdebe45ed3432
Alexander Krupenkin <mail@akru.me>
2017-12-10 22:02:42 +0300

    Added market fee collect

--- contracts/builder/BuilderRobotLiability.sol
+++ contracts/builder/BuilderRobotLiability.sol
@@ -13,6 +13,19 @@ contract BuilderRobotLiability is Builder {
     ERC20 public constant utility = ERC20(0x5DF531240f97049ee8d28A8E51030A3b5a8e8CE4);
     ERC20 public constant weth    = ERC20(0xC00Fd9820Cd2898cC4C054B7bF142De637ad129A);
     bytes constant MSGPREFIX = "\x19Ethereum Signed Message:\n32";
+
+    mapping(bytes32 => uint256) marketFee;
+
+    function getMarketFee(bytes _model) public view returns (uint256)
+    { return marketFee[keccak256(_model)]; }
+
+    function setMarketFee(bytes _model, uint256 _fee) public onlyOwner
+    { marketFee[keccak256(_model)] = _fee; }
+
+    address public marketSupply;
+
+    function setMarketSupply(address _supply) public onlyOwner
+    { marketSupply = _supply; }
     
     /**
      * @dev Create robot liability contract
@@ -40,6 +53,8 @@ contract BuilderRobotLiability is Builder {
         var inst = CreatorRobotLiability.create(
 						_model, _objective, promisee, promisor, _param[0], _param[1], _param[2]);
         require(weth.transferFrom(promisee, inst, _param[0] * _param[1]));
+        require(weth.transferFrom(promisee, marketSupply,
+                                  _param[0] * _param[1] * marketFee[keccak256(_model)] / 100));
         require(utility.transferFrom(promisor, inst, _param[2]));
 
         getContractsOf[msg.sender].push(inst);
